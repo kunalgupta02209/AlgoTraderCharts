7fbcda1c7aa5c0fa99402f35b0f3770a6091d5c7
Kunal Gupta <kunalgupta02209@gmail.com>
2021-01-21 17:31:28 +0530

    moved conf file

--- chart_server/static/config/config.py
+++ chart_server/static/config/config.py
@@ -1,6 +1,6 @@
 mongodb_url = ""
 from pathlib import Path
-config_file = Path('./chart_server/static/config/db.conf')
+config_file = Path('db.conf')
 with open(config_file) as f:
     mongodb_url = f.readline().strip()
 timezone = 'Asia/Calcutta'
